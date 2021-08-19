5dab1e1a63996770be47f7b6546628ec64868593
LukaszKotwica <lukasz2690@gmail.com>
2021-08-19 13:37:54 +0200

    Logowanie

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  #get 'admin/index'
+  #get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  root 'admin#index'
+
+end
